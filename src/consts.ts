de27801c89487af09b4ff0adc37065fcf13be566
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-05-17 14:28:39 +0500

    move to lipu.pona.la

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "kulupu-lapo.github.io/open";
-export const SITE_TITLE = "open Lapo";
-export const SITE_DESCRIPTION = "A test frontend for the Lapo library.";
+export const SITE_URL = "lipu.pona.la";
+export const SITE_TITLE = "lipu.pona.la";
+export const SITE_DESCRIPTION =
+  "lipu pona, the Toki Pona library by kulupu Lapo.";
