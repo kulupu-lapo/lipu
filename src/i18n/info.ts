d456c4858f4c60fae70e59176682c8e1d8b92479
quirked-up <115190871+quirked-up@users.noreply.github.com>
2025-06-07 05:56:58 +1200

    prepare footer for translation

---
+++ src/i18n/info.ts
@@ -0,0 +1,14 @@
+// this will be parsed as raw html.
+// please make sure all content is safe, and does not depend on user input
+
+export const data = {
+    en: {
+        footer: `Library put together by kala Asi, jan Juwan, ijo vivi, jan Kita, and others.<br>
+        Licenses for articles specified individually.<br>
+        Website made by <a href="https://kala-asi.li.pona.la">kala Asi</a>.<br>
+        Source code licensed under <a href="https://www.gnu.org/licenses/gpl-3.0.html">GPL-3.0</a>
+        and hosted on <a href="https://github.com/kulupu-lapo/lipu">GitHub</a>.`
+    },
+    tok: {
+    }
+}
