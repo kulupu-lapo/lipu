b49958a1c2d6329fff8061c1140335434e6f0c21
quirked-up <115190871+quirked-up@users.noreply.github.com>
2025-06-07 06:21:01 +1200

    prepare metadata section for translation

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,6 +7,42 @@ export const data = {
     'nav.adaptations': "Adaptations",
     'nav.random': "Random",
     'nav.add': "Add",
+
+
+    'metadata.container_title': "Details",
+
+    'metadata.github.pre': "Edit on ",
+    'metadata.github.github': "Github",
+    'metadata.github.post': "",
+
+    'metadata.description.pre': "Description: ",
+    'metadata.description.post': "",
+
+    'metadata.license.pre': "License: ",
+    'metadata.license.post': "",
+
+    'metadata.proofreaders.pre': "Proofreaders: ",
+    'metadata.proofreaders.between': ", ",
+    'metadata.proofreaders.post': "",
+
+    'metadata.tags.pre': "Tags: ",
+    'metadata.tags.post': "",
+
+    'metadata.sources.pre': "Sources: ",
+    'metadata.sources.post': "",
+
+    'metadata.archives.pre': "Archives: ",
+    'metadata.archives.post': "",
+
+    'metadata.original.pre': "Original: ",
+    'metadata.original.post': "",
+    'metadata.originalauthors.pre': "by ",
+    'metadata.originalauthors.between': ", ",
+    'metadata.originalauthors.post': "",
+
+    'metadata.notes.pre': "Notes:",
+    'metadata.notes.post': "",
+
   },
   tok: {
     'nav.all': "lipu",
