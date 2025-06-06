392256b2459875c855bea8fa77d11ce0e8f24cdf
quirked-up <115190871+quirked-up@users.noreply.github.com>
2025-06-07 06:58:01 +1200

    prepare rest of index for translation

---
+++ src/i18n/site.ts
@@ -0,0 +1,10 @@
+export const data = {
+    en: {
+        'index.h2': "Contributing and project overview",
+        'index.contributors.title': "Hall of fame",
+        'index.contributors.alttext': "poki Lapo contributors' profile pictures",
+        'index.contributors.caption': "Thank you all so much, wouldn't be possible without you! ❤️"
+    },
+    tok: {
+    },
+  } as const;
