3d3ce1e9aeffd4acb4c9a6db76461596b4ac3a3f
quirked-up <115190871+quirked-up@users.noreply.github.com>
2025-06-07 04:03:23 +1200

    move all content pages under /[...lang]/ subfolder

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -4,3 +4,26 @@ export const languages = {
 };
 export const locales = Object.keys(languages);
 export const defaultLang = 'en';
+
+const i18n_prefixes = locales.map( locale => ({ locale: locale, prefix: locale }) ).concat({ locale: defaultLang, prefix: '' });
+
+export async function getStaticLanguagePaths() {
+  return i18n_prefixes.map(({locale, prefix}) => {
+    return { params: { lang: prefix }, props: { lang: locale }, };
+  });
+}
+
+export function internationaliseStaticPaths<T,E>(paths : {params: T, props: E}[]) : {params : T & {lang: string}, props: E & {lang: string}}[] {
+    return paths.flatMap(({ params, props }) => 
+        i18n_prefixes.map(({locale, prefix})=>({
+            params: {
+                lang: prefix,
+                ...params
+            },
+            props: {
+                lang: locale,
+                ...props
+            }
+        }))
+    )
+} 
