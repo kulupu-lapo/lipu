cc09fa798050496f980d6c0acdfa809f07cd6542
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-10 16:07:04 +0500

    refactor: remove a hardcoded url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,34 +2,32 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import { locales, defaultLang } from "./src/utils/i18n.ts"
+import { locales, defaultLang } from "./src/utils/i18n.ts";
+import { SITE_URL } from "./src/consts";
 
 const deploy = import.meta.env.PROD
-  ? { site: "https://lipu.pona.la/", base: "./" }
-  : { site: "http://localhost/", base: "./" };
+  ? { site: `https://${SITE_URL}/` }
+  : { site: "http://localhost/" };
 
 // https://astro.build/config
 export default defineConfig({
   ...deploy,
-  ...{
-    integrations: [
-      // mdx(),
-      sitemap({
-        i18n: {
-          locales : Object.fromEntries(locales.map(x=>[x,x])),
-          defaultLocale: defaultLang,
-        }
-      }),
-      // react(),
-    ],
-  },
+  integrations: [
+    // mdx(),
+    sitemap({
+      i18n: {
+        locales: Object.fromEntries(locales.map((x) => [x, x])),
+        defaultLocale: defaultLang,
+      },
+    }),
+    // react(),
+  ],
   i18n: {
     locales,
     defaultLocale: defaultLang,
     routing: {
-        prefixDefaultLocale: false,
-        redirectToDefaultLocale: true,
-    }
+      prefixDefaultLocale: false,
+      redirectToDefaultLocale: true,
+    },
   },
 });
